e9b032130ad816b48e52f165bfcdfdb4c4e140f2
Genar Trias Ortiz <gtrias@gmail.com>
2023-12-18 15:32:09 +0100

    store all datbase in a variable

--- main.ts
+++ main.ts
@@ -14,7 +14,8 @@ export default class OllamaPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadGraph();
+		const graph = await this.loadGraph();
+		console.log("graph: ", graph)
 
 		await this.loadSettings();
 
@@ -91,40 +92,47 @@ export default class OllamaPlugin extends Plugin {
 	async loadGraph() {
 		console.log("Loading graph");
 
-		const { appId } = window.global.app
+		const { appId } = window.global.app;
 
-		let db;
-		const request = indexedDB.open(`${appId}-cache`);
+		return new Promise((resolve, reject) => {
+			let db;
+			const request = indexedDB.open(`${appId}-cache`);
 
-		request.onerror = (event) => {
-			console.error("Error opening database");
-			console.error(event);
-		}
-		request.onsuccess = async (event) => {
-			console.log("Success!");
-			db = event.target.result;
-
-			// Iterate all the data in the database
-			const objectStore = await db.transaction("file").objectStore("file");
-
-			objectStore.openCursor().onsuccess = (event) => {
-				const cursor = event.target.result;
-				if (cursor) {
-					console.log("Name for SSN " + cursor.key + " is " + JSON.stringify(cursor.value));
-
-					const metadataObjectStore = db.transaction("metadata").objectStore("metadata");
-					const request = metadataObjectStore.get(cursor.value.hash)
-					request.onsuccess = (event) => {
-						const metadata = event.target.result;
-						console.log(metadata)
-					}
-
-					cursor.continue();
-				} else {
-					console.log("No more entries!");
-				}
+			request.onerror = (event) => {
+				console.error("Error opening database");
+				console.error(event);
+				reject(event);
 			};
-		}
+			request.onsuccess = async (event) => {
+				console.log("Success!");
+				db = event.target.result;
+
+				const results = [];
+
+				const objectStore = await db.transaction("file").objectStore("file");
+
+				objectStore.openCursor().onsuccess = (event) => {
+					const cursor = event.target.result;
+					if (cursor) {
+						// console.log("Name for SSN " + cursor.key + " is " + JSON.stringify(cursor.value));
+
+						const metadataObjectStore = db.transaction("metadata").objectStore("metadata");
+						const request = metadataObjectStore.get(cursor.value.hash);
+						request.onsuccess = (event) => {
+							const metadata = event.target.result;
+							// console.log(metadata);
+							const result = { file: cursor.key, ...metadata }; // Append file key to metadata
+							results.push(result); // Store the result in the array
+						};
+
+						cursor.continue();
+					} else {
+						console.log("No more entries!");
+						resolve(results); // Resolve the promise with the results array
+					}
+				};
+			};
+		});
 	}
 
 	async saveSettings() {
